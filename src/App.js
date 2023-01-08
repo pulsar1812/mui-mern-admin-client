acf3f7421f75342a24b749d0a22a732147a9a5bc
pulsar1812 <cluster000@gmail.com>
2023-01-08 22:36:46 +0800

    Add folder structure, theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <div className='app'></div>
-}
+import { useMemo } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
 
-export default App
+import { themeSettings } from './theme'
+import Dashboard from './scenes/bashboard'
+import Layout from './scenes/layout'
+
+export default function App() {
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+
+  return (
+    <div className='app'>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path='/' element={<Navigate to='/dashboard' replace />} />
+              <Route path='/dashboard' element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  )
+}
